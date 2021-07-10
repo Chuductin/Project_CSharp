42599a1c02832077451918c76cd947f1d5bc8d5e
DucTin <chuductintl@gmail.com>
2021-07-10 17:16:13 +0700

    Add all

---
+++ PROJECT CSharp/Assets/js/profile.js
@@ -0,0 +1,384 @@
+function edit() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    viewSelect.style.setProperty('margin-left', '0px');
+    viewSelect.style.setProperty('border-left', '0px');
+    viewSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '0px');
+    manageCatSelect.style.setProperty('border-left', '0px');
+    manageCatSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '0px');
+    manageVouSelect.style.setProperty('border-left', '0px');
+    manageVouSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '0px');
+    manageAccSelect.style.setProperty('border-left', '0px');
+    manageAccSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '0px');
+    manageProductSelect.style.setProperty('border-left', '0px');
+    manageProductSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '0px');
+    changeSelect.style.setProperty('border-left', '0px');
+    changeSelect.style.setProperty('background-color', 'white');
+
+    editSelect.style.setProperty('margin-left', '20px');
+    editSelect.style.setProperty('border-left', '5px solid #f5d730');
+    editSelect.style.setProperty('background-color', '#f8f7f5');
+    if (edit.style.display == "none") {
+        edit.style.display = "block";
+        change.style.display = "none";
+        view.style.display = "none";
+        manageProduct.style.display = "none";
+        manageCat.style.display = "none";
+        manageVou.style.display = "none";
+        manageAcc.style.display = "none";
+    }
+}
+
+function viewOrder() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    editSelect.style.setProperty('margin-left', '0px');
+    editSelect.style.setProperty('border-left', '0px');
+    editSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '0px');
+    manageCatSelect.style.setProperty('border-left', '0px');
+    manageCatSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '0px');
+    manageVouSelect.style.setProperty('border-left', '0px');
+    manageVouSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '0px');
+    manageAccSelect.style.setProperty('border-left', '0px');
+    manageAccSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '0px');
+    manageProductSelect.style.setProperty('border-left', '0px');
+    manageProductSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '0px');
+    changeSelect.style.setProperty('border-left', '0px');
+    changeSelect.style.setProperty('background-color', 'white');
+
+    viewSelect.style.setProperty('margin-left', '20px');
+    viewSelect.style.setProperty('border-left', '5px solid #f5d730');
+    viewSelect.style.setProperty('background-color', '#f8f7f5');
+    if (view.style.display == "none") {
+        view.style.display = "block";
+        change.style.display = "none";
+        edit.style.display = "none";
+        manageProduct.style.display = "none";
+        manageCat.style.display = "none";
+        manageVou.style.display = "none";
+        manageAcc.style.display = "none";
+    }
+}
+
+function changePass() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    editSelect.style.setProperty('margin-left', '0px');
+    editSelect.style.setProperty('border-left', '0px');
+    editSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '0px');
+    manageCatSelect.style.setProperty('border-left', '0px');
+    manageCatSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '0px');
+    manageVouSelect.style.setProperty('border-left', '0px');
+    manageVouSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '0px');
+    manageAccSelect.style.setProperty('border-left', '0px');
+    manageAccSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '0px');
+    manageProductSelect.style.setProperty('border-left', '0px');
+    manageProductSelect.style.setProperty('background-color', 'white');
+
+    viewSelect.style.setProperty('margin-left', '0px');
+    viewSelect.style.setProperty('border-left', '0px');
+    viewSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '20px');
+    changeSelect.style.setProperty('border-left', '5px solid #f5d730');
+    changeSelect.style.setProperty('background-color', '#f8f7f5');
+    if (change.style.display == "none") {
+        change.style.display = "block";
+        view.style.display = "none";
+        edit.style.display = "none";
+        manageProduct.style.display = "none";
+        manageCat.style.display = "none";
+        manageVou.style.display = "none";
+        manageAcc.style.display = "none";
+    }
+}
+
+function manageProduct() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    editSelect.style.setProperty('margin-left', '0px');
+    editSelect.style.setProperty('border-left', '0px');
+    editSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '0px');
+    manageCatSelect.style.setProperty('border-left', '0px');
+    manageCatSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '0px');
+    manageVouSelect.style.setProperty('border-left', '0px');
+    manageVouSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '0px');
+    manageAccSelect.style.setProperty('border-left', '0px');
+    manageAccSelect.style.setProperty('background-color', 'white');
+
+    viewSelect.style.setProperty('margin-left', '0px');
+    viewSelect.style.setProperty('border-left', '0px');
+    viewSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '0px');
+    changeSelect.style.setProperty('border-left', '0px');
+    changeSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '20px');
+    manageProductSelect.style.setProperty('border-left', '5px solid #f5d730');
+    manageProductSelect.style.setProperty('background-color', '#f8f7f5');
+    if (manageProduct.style.display == "none") {
+        manageProduct.style.display = "block";
+        view.style.display = "none";
+        edit.style.display = "none";
+        change.style.display = "none";
+        manageCat.style.display = "none";
+        manageVou.style.display = "none";
+        manageAcc.style.display = "none";
+    }
+}
+
+function manageCategory() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    editSelect.style.setProperty('margin-left', '0px');
+    editSelect.style.setProperty('border-left', '0px');
+    editSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '0px');
+    changeSelect.style.setProperty('border-left', '0px');
+    changeSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '0px');
+    manageVouSelect.style.setProperty('border-left', '0px');
+    manageVouSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '0px');
+    manageAccSelect.style.setProperty('border-left', '0px');
+    manageAccSelect.style.setProperty('background-color', 'white');
+
+    viewSelect.style.setProperty('margin-left', '0px');
+    viewSelect.style.setProperty('border-left', '0px');
+    viewSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '0px');
+    manageProductSelect.style.setProperty('border-left', '0px');
+    manageProductSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '20px');
+    manageCatSelect.style.setProperty('border-left', '5px solid #f5d730');
+    manageCatSelect.style.setProperty('background-color', '#f8f7f5');
+    if (manageCat.style.display == "none") {
+        manageCat.style.display = "block";
+        view.style.display = "none";
+        edit.style.display = "none";
+        change.style.display = "none";
+        manageProduct.style.display = "none";
+        manageVou.style.display = "none";
+        manageAcc.style.display = "none";
+    }
+}
+
+function manageVoucher() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    editSelect.style.setProperty('margin-left', '0px');
+    editSelect.style.setProperty('border-left', '0px');
+    editSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '0px');
+    changeSelect.style.setProperty('border-left', '0px');
+    changeSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '0px');
+    manageCatSelect.style.setProperty('border-left', '0px');
+    manageCatSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '0px');
+    manageAccSelect.style.setProperty('border-left', '0px');
+    manageAccSelect.style.setProperty('background-color', 'white');
+
+    viewSelect.style.setProperty('margin-left', '0px');
+    viewSelect.style.setProperty('border-left', '0px');
+    viewSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '0px');
+    manageProductSelect.style.setProperty('border-left', '0px');
+    manageProductSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '20px');
+    manageVouSelect.style.setProperty('border-left', '5px solid #f5d730');
+    manageVouSelect.style.setProperty('background-color', '#f8f7f5');
+    if (manageVou.style.display == "none") {
+        manageVou.style.display = "block";
+        view.style.display = "none";
+        edit.style.display = "none";
+        change.style.display = "none";
+        manageProduct.style.display = "none";
+        manageCat.style.display = "none";
+        manageAcc.style.display = "none";
+    }
+}
+
+function manageAccout() {
+    var edit = document.getElementById('editForm');
+    var view = document.getElementById('historyOrder');
+    var change = document.getElementById('changePassword');
+    var manageProduct = document.getElementById('manageProduct');
+    var manageCat = document.getElementById('manageCate');
+    var manageVou = document.getElementById('manageVoucher');
+    var manageAcc = document.getElementById('manageAccount');
+
+    var viewSelect = document.getElementById('viewOrder');
+    var changeSelect = document.getElementById('changePass');
+    var editSelect = document.getElementById('editprofile');
+    var manageProductSelect = document.getElementById('manageP');
+    var manageCatSelect = document.getElementById('manageCat');
+    var manageVouSelect = document.getElementById('manageVou');
+    var manageAccSelect = document.getElementById('manageAcc');
+
+    editSelect.style.setProperty('margin-left', '0px');
+    editSelect.style.setProperty('border-left', '0px');
+    editSelect.style.setProperty('background-color', 'white');
+
+    changeSelect.style.setProperty('margin-left', '0px');
+    changeSelect.style.setProperty('border-left', '0px');
+    changeSelect.style.setProperty('background-color', 'white');
+
+    manageCatSelect.style.setProperty('margin-left', '0px');
+    manageCatSelect.style.setProperty('border-left', '0px');
+    manageCatSelect.style.setProperty('background-color', 'white');
+
+    manageVouSelect.style.setProperty('margin-left', '0px');
+    manageVouSelect.style.setProperty('border-left', '0px');
+    manageVouSelect.style.setProperty('background-color', 'white');
+
+    viewSelect.style.setProperty('margin-left', '0px');
+    viewSelect.style.setProperty('border-left', '0px');
+    viewSelect.style.setProperty('background-color', 'white');
+
+    manageProductSelect.style.setProperty('margin-left', '0px');
+    manageProductSelect.style.setProperty('border-left', '0px');
+    manageProductSelect.style.setProperty('background-color', 'white');
+
+    manageAccSelect.style.setProperty('margin-left', '20px');
+    manageAccSelect.style.setProperty('border-left', '5px solid #f5d730');
+    manageAccSelect.style.setProperty('background-color', '#f8f7f5');
+    if (manageAcc.style.display == "none") {
+        manageAcc.style.display = "block";
+        view.style.display = "none";
+        edit.style.display = "none";
+        change.style.display = "none";
+        manageProduct.style.display = "none";
+        manageCat.style.display = "none";
+        manageVou.style.display = "none";
+    }
+}
---
+++ PROJECT CSharp/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJECT_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your Login page.";
+
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Your Profile page.";
+
+            return View();
+        }
+
+        public ActionResult OrderDetail()
+        {
+            ViewBag.Message = "Your OrderDetail page.";
+
+            return View();
+        }
+
+        public ActionResult ProductGrid()
+        {
+            ViewBag.Message = "Your ProductGrid page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your Register page.";
+
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            ViewBag.Message = "Your ProductDetail page.";
+
+            return View();
+        }
+
+        public ActionResult AccountDetail()
+        {
+            ViewBag.Message = "Your AccountDetail page.";
+
+            return View();
+        }
+
+        public ActionResult VoucherDetail()
+        {
+            ViewBag.Message = "Your VoucherDetail page.";
+
+            return View();
+        }
+
+        public ActionResult CategoryDetail()
+        {
+            ViewBag.Message = "Your CategoryDetail page.";
+
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            ViewBag.Message = "Your Card page.";
+
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            ViewBag.Message = "Your Checkout page.";
+
+            return View();
+        }
+
+        public ActionResult SingleProduct()
+        {
+            ViewBag.Message = "Your SingleProduct page.";
+
+            return View();
+        }
+    }
+}
